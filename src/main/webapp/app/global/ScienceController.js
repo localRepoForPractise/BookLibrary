0b24e458c880f1932aa96a5471368f92319a2ef3
SMORE11 <SMORE11@01HW596068.eu1.jlrint.com>
2019-06-12 13:22:49 +0100

    Sciencebook data displayed

--- src/main/webapp/app/global/ScienceController.js
+++ src/main/webapp/app/global/ScienceController.js
@@ -4,65 +4,36 @@
 (function() {
 
 	var app = angular.module('bookLibraryApp');
-	app.controller('mainController', function($rootScope, $scope, $http, HttpService, $location) {
+	app.controller('scienceControllers', function($rootScope, $scope, $http, HttpService, $location) {
 		
-		/** TO GET BASE URI OF APP*//
+		/** TO GET BASE URI OF APP*/
 		$rootScope.baseUrl = $location.$$protocol + "://"+$location.$$host + ":"+$location.$$port + "/BookLibrary/";
 		$scope.connection='';
 		
-		/**TO Check connection with DataBase
-		 * */
-		function checkConnection() {
-			HttpService.GET($rootScope.baseUrl + 'checkConnection').then(
-					function successCallback(response) {
-						/**console.log("Connection: ", response);*/
-						$scope.connection = "Connection: " + response.message;
-					});
-		};
-		checkConnection();
-		function ScienceController($scope) {
-		    
-		    $scope.Science = [
-		        {Book_Id:101,Category_Id:1,Book_Name:'Elegant Universe ',Book_Author:'Brian Greene ',Created_By:'smore11',
-		        	Created_Date:'12/06/19'
-		        }
-		        }
-		    ];
-	/*	$scope.submit= function (testVariable){
-			
-			var reqData = testVariable;
-			HttpService.POST($rootScope.baseUrl + 'testVariable',reqData).then(
+		$scope.IsVisible = false;
+        $scope.ScienceBooks = function () {
+             $scope.Books = [
+            	{Book_Id:101,Category_Id:1,Book_Name:'Elegant Universe ',Book_Author:'Brian Greene ',Created_By:'smore11',Created_Date:'12/06/19'},
+                {Book_Id:102,Category_Id:1,Book_Name:'Artificial Intelligence',Book_Author:' Melanie Mitchell',Created_By:'smore11',Created_Date:'12/06/19'},
+                {Book_Id:103,Category_Id:1,Book_Name:'Know-It-All Society',Book_Author:' Michael P. Lynch',Created_By:'smore11',Created_Date:'12/06/19'},
+             ], 
+            $scope.IsVisible = true;
+        };		
+		
+        $scope.ScienceBooks();
+        
+		/*$scope.ScienceBooks=function (Science)
+		{
+			var reqData=Science;
+			HttpService.POST($rootScope.baseUrl + 'Science',reqData).then(
 					function successCallback(response) {
 						if(response.code==="200")
 							console.log("SUCCESS");
 						else if(response.code==="500")
 							console.log("FAILURE");
-						
-						*//***console.log("Connection: ", response);*//*
-						//$scope.connection = "Connection: " + response.message;
-					});
-					function ScienceController($scope) {
-								    
-								    $scope.Science = [
-								        {Book_Id:101,Category_Id:1,Book_Name:'Elegant Universe ',Book_Author:'Brian Greene ',Created_By:'smore11',
-								        	Created_Date:'12/06/19'
-								        }
-								        }
-								    ];
-			//bookLiberary
-			
-			$scope.ScienceBooks=function (Science)
-			{
-				var reqData=Science;
-				HttpService.POST($rootScope.baseUrl + 'Science',reqData).then(
-						function successCallback(response) {
-							if(response.code==="200")
-								console.log("SUCCESS");
-							else if(response.code==="500")
-								console.log("FAILURE"*/);
-			
-								
-				}
 			}
-		}
+		};
+		*/
+		
 	});
+}());
