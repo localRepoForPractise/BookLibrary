ab497ec423e3e9d348a37d513b3bb7007bf1e5c3
pdawari <pdawari@pdawari@partner.jaguarlandrover.com>
2019-06-13 14:20:44 +0100

    Updated

---
+++ src/main/webapp/app/UserBookHistory/UserBookHistoryController.js
@@ -0,0 +1,13 @@
+///**
+//* Main Controller of Book Library App
+//*/
+//(function() {
+//
+//	var app = angular.module('bookLibraryApp');
+//	app.controller('mainController', function($rootScope, $scope, $http, HttpService, $location) {
+//		
+//		/** TO GET BASE URI OF APP*/
+//		$rootScope.baseUrl = $location.$$protocol + "://"+$location.$$host + ":"+$location.$$port + "/BookLibrary/";
+//		$scope.connection='';
+//		
+//	});
